d2b840f5716f2f46a17dda224c1188aed233b859
adityaojha0609 <aadityaojha0609@gmail.com>
2026-08-08 12:24:45 +0530

    Time: 0 ms (100%), Space: 52.8 MB (78.3%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,42 +1,24 @@
 class Solution {
-    public int singleNonDuplicate(int[] A) {
-        int n = A.length;
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
 
-        if (n == 1) return A[0];
+        while (low < high) {
+            int mid = low + (high - low) / 2;
 
-        int st = 0;
-        int end = n - 1;
+            // Make mid even
+            if (mid % 2 == 1) {
+                mid--;
+            }
 
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            // First element
-            if (mid == 0 && A[0] != A[1])
-                return A[mid];
-
-            // Last element
-            if (mid == n - 1 && A[n - 1] != A[n - 2])
-                return A[mid];
-
-            // Unique element found
-            if (A[mid - 1] != A[mid] && A[mid + 1] != A[mid])
-                return A[mid];
-
-            if (mid % 2 == 0) { // even index
-                if (A[mid - 1] == A[mid]) {
-                    end = mid - 1;
-                } else {
-                    st = mid + 1;
-                }
-            } else { // odd index
-                if (A[mid - 1] == A[mid]) {
-                    st = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+            // Pair is correct: nums[mid] == nums[mid + 1]
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
             }
         }
 
-        return -1;
+        return nums[low];
     }
 }
