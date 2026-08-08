2ccd7d94c0ac92a8e1fe970182c3262063303987
adityaojha0609 <aadityaojha0609@gmail.com>
2026-08-08 12:23:15 +0530

    Time: 0 ms (100%), Space: 52.9 MB (59.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,17 +1,42 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(int i = 0;i<n;i++){
-            if(map.get(nums[i])==1){
-            return nums[i];
+    public int singleNonDuplicate(int[] A) {
+        int n = A.length;
+
+        if (n == 1) return A[0];
+
+        int st = 0;
+        int end = n - 1;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            // First element
+            if (mid == 0 && A[0] != A[1])
+                return A[mid];
+
+            // Last element
+            if (mid == n - 1 && A[n - 1] != A[n - 2])
+                return A[mid];
+
+            // Unique element found
+            if (A[mid - 1] != A[mid] && A[mid + 1] != A[mid])
+                return A[mid];
+
+            if (mid % 2 == 0) { // even index
+                if (A[mid - 1] == A[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            } else { // odd index
+                if (A[mid - 1] == A[mid]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
+
         return -1;
-        
-        
     }
 }
